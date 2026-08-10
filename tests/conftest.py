427ee9558d4801f2862e88461d81f6ee20cce005
Handy-Harry <44102886+Handy-Harry@users.noreply.github.com>
2026-08-10 17:21:52 +0200

    Configure Home Assistant async tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,8 @@
 """Shared GridPilot test fixtures."""
 
 from contextlib import suppress
-from importlib.util import find_spec
 
 import pytest
-
-pytest_plugins = (
-    ["pytest_homeassistant_custom_component.plugins"]
-    if find_spec("pytest_homeassistant_custom_component")
-    else []
-)
 
 
 @pytest.fixture(autouse=True)
